3187ce566e424f786788063fd5178b20faf80118
Danilove <dansousa09@gmail.com>
2024-08-01 14:02:34 -0300

    refactor(core): Remove unused code

--- src/main/java/com/danilove/emailservice/core/exceptions/EmailServiceException.java
+++ src/main/java/com/danilove/emailservice/core/exceptions/EmailServiceException.java
@@ -1,11 +1,6 @@
 package com.danilove.emailservice.core.exceptions;
 
 public class EmailServiceException extends RuntimeException {
-
-    public EmailServiceException(String message) {
-        super(message);
-    }
-
     public EmailServiceException(String message, Throwable cause) {
         super(message, cause);
     }
