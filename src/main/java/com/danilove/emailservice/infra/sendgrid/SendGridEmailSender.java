d3ea15b3b3a35c3922d8581418854366de34ca47
Danilove <dansousa09@gmail.com>
2024-08-01 13:50:05 -0300

    feat(infra): Create the service to implements the gateway and use send grid sdk to perfome

---
+++ src/main/java/com/danilove/emailservice/infra/sendgrid/SendGridEmailSender.java
@@ -0,0 +1,36 @@
+package com.danilove.emailservice.infra.sendgrid;
+
+import com.danilove.emailservice.adapters.EmailSenderGateway;
+import com.danilove.emailservice.core.exceptions.EmailServiceException;
+import com.sendgrid.*;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.logging.Logger;
+
+@Service
+public class SendGridEmailSender implements EmailSenderGateway {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(SendGridEmailSender.class);
+    private final Logger logger = Logger.getLogger(SendGridEmailSender.class.getName());
+
+    private final SendGrid sg = new SendGrid("SENDGRID_API_KEY");
+
+    @Override
+    public void send(String to, String subject, String body) {
+        Email from = new Email("sender@mail.com");
+        Email toEmail = new Email(to);
+        Content content = new Content("text/plain", body);
+        Mail mail = new Mail(from, subject, toEmail, content);
+
+        Request request = new Request();
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+            logger.info(response.getBody());
+        } catch (Exception e) {
+            throw new EmailServiceException("Error sending email", e);
+        }
+    }
+}
